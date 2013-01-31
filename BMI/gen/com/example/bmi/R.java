21dabf01b7c353d47fdbc96fa3ee77a8a05e08fb
lisalin <lishan914@gmail.com>
2013-01-31 18:18:48 +0800

    add dialog and toast

--- BMI/gen/com/example/bmi/R.java
+++ BMI/gen/com/example/bmi/R.java
@@ -28,15 +28,19 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
+        public static final int about_msg=0x7f04000b;
+        public static final int about_title=0x7f04000a;
         public static final int advice_average=0x7f040001;
         public static final int advice_heavy=0x7f040002;
         public static final int advice_light=0x7f040000;
         public static final int app_name=0x7f040003;
         public static final int bmi_result=0x7f040009;
         public static final int button_text=0x7f040008;
+        public static final int dialog_OK=0x7f04000c;
         public static final int height_text=0x7f040006;
         public static final int hello_world=0x7f040004;
         public static final int menu_settings=0x7f040005;
+        public static final int toast_text=0x7f04000d;
         public static final int weight_text=0x7f040007;
     }
     public static final class style {
--- BMI/src/com/example/bmi/MainActivity.java
+++ BMI/src/com/example/bmi/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -17,24 +18,24 @@ public class MainActivity extends Activity {
 	private EditText fieldheight;
 	private EditText fieldweight;
 	private TextView view_result;
-    private TextView view_suggest;
-	
+	private TextView view_suggest;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		findViews();
-	    setListeners(); 
+		setListeners();
 	}
 
 	private void findViews() {
 		calcbutton = (Button) findViewById(R.id.submit);
 		fieldheight = (EditText) findViewById(R.id.height);
-	    fieldweight = (EditText) findViewById(R.id.weight);
-	    
-	    view_result = (TextView) findViewById(R.id.result);
-        view_suggest = (TextView) findViewById(R.id.suggest);
+		fieldweight = (EditText) findViewById(R.id.weight);
+
+		view_result = (TextView) findViewById(R.id.result);
+		view_suggest = (TextView) findViewById(R.id.suggest);
 	}
 
 	private void setListeners() {
@@ -44,7 +45,7 @@ public class MainActivity extends Activity {
 	private Button.OnClickListener calcBMI = new OnClickListener() {
 		public void onClick(View v) {
 			DecimalFormat nf = new DecimalFormat("0.00");
-			
+
 			double height = Double
 					.parseDouble(fieldheight.getText().toString()) / 100;
 			double weight = Double
@@ -61,6 +62,28 @@ public class MainActivity extends Activity {
 			} else {
 				view_suggest.setText(R.string.advice_average);
 			}
+			openOptionsDialog();
+		}
+
+		private void openOptionsDialog() {
+			// this is Dialog
+//			AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+//			dialog.setTitle(R.string.about_title);
+//			dialog.setMessage(R.string.about_msg);
+//			
+//			dialog.setPositiveButton(R.string.dialog_OK,
+//                    new DialogInterface.OnClickListener(){
+//                        public void onClick(
+//                                DialogInterface dialoginterface, int i){
+//                                }
+//                        });
+//			
+//			dialog.show();
+
+			// this is Toast
+			Toast popup = Toast.makeText(MainActivity.this,
+					R.string.toast_text, Toast.LENGTH_SHORT);
+			popup.show();
 		}
 	};
 
